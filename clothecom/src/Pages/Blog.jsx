ca6b308d777e7a771270d8e3d6f1e5cba275d6c0
mdfaizan973 <faizan.md9735@gmail.com>
2024-03-05 18:31:05 +0530

    added blogs

--- clothecom/src/Pages/Blog.jsx
+++ clothecom/src/Pages/Blog.jsx
@@ -3,23 +3,23 @@ import styled from "styled-components";
 export default function Blog() {
   const blogs = [
     {
-      img: "",
+      img: "https://img1.junaroad.com//assets/images/mobileNotif/img-1703503964040.jpg?crsl_pos=3",
       disc: "Explore the world of fashion with our exquisite collection of clothing that blends style and comfort seamlessly. Our range includes trendy tops, elegant dresses, fashionable skirts, and chic pants, all crafted from high-quality fabrics to ensure durability and a perfect fit. Whether you're dressing up for a special occasion or keeping it casual for everyday wear, our clothing line has something for every wardrobe. With attention to detail and a passion for design, each piece is carefully curated to reflect the latest trends while maintaining a timeless appeal. Elevate your style and express your individuality with our stunning collection of clothes.",
       title: "Trendy Tops: Elevate Your Style with Our Latest Collection.",
     },
     {
-      img: "",
+      img: "https://img1.junaroad.com//assets/images/mobileNotif/img-1708346698411.jpg?crsl_pos=3",
       disc: "Elevate your style with our latest collection of trendy and fashionable clothes. From chic dresses to comfy loungewear, we have everything you need to stay on top of the fashion game. Explore our versatile range of tops, bottoms, outerwear, and accessories designed to effortlessly enhance your wardrobe. Whether you're dressing up for a special occasion or keeping it casual for everyday wear, our high-quality fabrics and attention to detail ensure both comfort and style. Embrace your unique personality and express yourself through our diverse selection of colors, patterns, and designs. Shop now and elevate your wardrobe today!",
       title: "Chic and Comfortable: Explore Our Stylish Bottoms Collection.",
     },
     {
-      img: "",
+      img: "https://img1.junaroad.com//assets/images/mobileNotif/img-1708930303049.jpg?crsl_pos=3",
       disc: "Indulge in the allure of fashion with our captivating collection of clothing. Embrace your unique style with our diverse range, from elegant dresses to trendy tops and chic bottoms. Crafted from premium fabrics, each piece is designed to provide both comfort and style, ensuring you look and feel confident all day long. Whether you're aiming for a casual, laid-back vibe or a sophisticated, polished look, our collection has something for every taste. Elevate your wardrobe with our exquisite clothing and make a statement with your fashion choices.",
       title:
         "Effortless Elegance: Discover the Perfect Dress for Every Occasion",
     },
     {
-      img: "",
+      img: "https://img1.junaroad.com//assets/images/mobileNotif/img-1705580304432.jpg?crsl_pos=3",
       disc: "Discover a world of fashion possibilities with our curated collection of clothing. From timeless classics to the latest trends, our range offers something for every style and occasion. Crafted from high-quality fabrics, our clothes are designed to be both stylish and comfortable, ensuring you look and feel your best. Whether you're dressing up for a special event or keeping it casual for everyday wear, our clothing is versatile enough to suit any wardrobe. Explore our collection today and find your perfect look.",
       title: "Fashion Forward: Stay on Trend with Our Latest Arrivals.",
     },
@@ -32,7 +32,7 @@ export default function Blog() {
       <div className="container">
         {blogs.map((ele, i) => (
           <div className="post" key={i}>
-            <img src="https://via.placeholder.com/800x400" alt="Post Image" />
+            <img src={ele.img} alt="Post Image" />
             <h2>{ele.title}</h2>
             <p>{ele.disc}</p>
             <a href="#" className="read-more">
@@ -57,9 +57,10 @@ const DIV = styled.div`
   }
 
   .post {
-    background-color: #fff;
-    border-radius: 5px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    border-radius: 20px;
     padding: 20px;
+    overflow: hidden;
     margin-bottom: 20px;
   }
 
